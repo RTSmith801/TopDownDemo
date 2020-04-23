d4587dac78cba77082a1a26a339f5465e889683d
Wreckeshey <RTSmith801@gmail.com>
2020-04-23 02:12:33 -0600

    Screen/Room Transitions - Making Progress
    Currently seem to be thrown off by hitting multiple colliders for entry and exit at the same time.

--- Assets/Scripts/RoomProperties.cs
+++ Assets/Scripts/RoomProperties.cs
@@ -10,7 +10,8 @@ public class RoomProperties : MonoBehaviour
     public SpriteRenderer blackOutCanvas;
     private Color transparent = new Color(1f, 1f, 1f, 0f);
     private Color opaque = new Color(1f, 1f, 1f, 1f);
-    private Color startingColor;    
+    private Color startingColor;
+    public List<SceneTransition> sceneTransitions = new List<SceneTransition>();
 
     // Start is called before the first frame update
     void Awake()
@@ -22,17 +23,20 @@ public class RoomProperties : MonoBehaviour
     {
         gm = FindObjectOfType<GameManager>();
         blackOutCanvas = GetComponent<SpriteRenderer>();
-        virtualCamera = GetComponentInChildren<CinemachineVirtualCamera>().gameObject;
+        virtualCamera = GetComponentInChildren<CinemachineVirtualCamera>().gameObject;        
     }
 
     private void Start()
     {
         blackOutCanvas.color = opaque;
-        virtualCamera.SetActive(false);                
+        virtualCamera.SetActive(false);
+        
+        
     }
     
     public void LeaveRoom()
     {
+        print("LeaveRoom has been called in " + transform.gameObject.name);
         virtualCamera.SetActive(false);        
         startingColor = blackOutCanvas.color;
         StartCoroutine(FadeBlackOutCanvas(startingColor, opaque, gm.fadeSpeed));
@@ -40,16 +44,18 @@ public class RoomProperties : MonoBehaviour
 
     public void EnterRoom()
     {
+        print("EnterRoom has been called in " + transform.gameObject.name);
         virtualCamera.SetActive(true);        
         startingColor = blackOutCanvas.color;
         StartCoroutine(FadeBlackOutCanvas(startingColor, transparent, gm.fadeSpeed));
     }
 
     IEnumerator FadeBlackOutCanvas(Color startingColor, Color fadeToColor, float fadeSpeed)
-    {        
+    {
         float currentTime = 0f;
-        while (startingColor != fadeToColor)
+        while (blackOutCanvas.color != fadeToColor)
         {
+            print(blackOutCanvas.color + " " + fadeToColor);
             currentTime += Time.deltaTime;
             blackOutCanvas.color = Color.Lerp(startingColor, fadeToColor, (currentTime / fadeSpeed));
             yield return null;
